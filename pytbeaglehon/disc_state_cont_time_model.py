917ddda2fa0085c3ff8961fc0842e6a3408ef510
Mark T. Holder <mtholder@gmail.com>
2010-10-16 19:30:24 -0500

    still not passing, but now calculating JC q_mat

--- pytbeaglehon/disc_state_cont_time_model.py
+++ pytbeaglehon/disc_state_cont_time_model.py
@@ -4,6 +4,11 @@
 """
 Wrapper around a discrete-state, continuous time model of character state change.
 """
+from itertools import izip
+from pytbeaglehon import get_logger
+from pytbeaglehon.parameter import MutableFloatParameter
+_LOG = get_logger(__name__)
+
 _EMPTY_SET = set()
 
 class DiscStateContTimeModel(object):
@@ -15,7 +20,7 @@ class DiscStateContTimeModel(object):
         self._asrv = kwargs.get('asrv')
         self._owns_calc_env = False
         self._changed_params = set()
-        self.q_mat = None
+        self._q_mat = None
         self._q_mat_hash = None
         self._prev_asrv_hash = None
         self._asrv_hash = None
@@ -26,6 +31,13 @@ class DiscStateContTimeModel(object):
         if param_list is not None:
             for p in param_list:
                 p.add_listener(self.param_changed)
+        self._changed_params.add(None) # having a non-empty set assures that the q_mat will be recognized as dirty
+
+    def calc_prob_matrices(self, edge_len, eigen_soln_caching=None, prob_mat_caching=None):
+        '''Returns a list containing a transition probability matrix for each rate category.'''
+        self._incarnate()
+        return self._calc_prob_matrices(edge_len, eigen_soln_caching=eigen_soln_caching, prob_mat_caching=prob_mat_caching)
+        
 
     def param_changed(self, p):
         '''Adds `p` to this list of changed_parameters.'''
@@ -40,7 +52,7 @@ class DiscStateContTimeModel(object):
         return self._char_type.num_states
     num_states = property(get_num_states)
 
-    def q_mat_is_dirty():
+    def q_mat_is_dirty(self):
         return (self._changed_params != _EMPTY_SET)
 
     def asrv_is_dirty():
@@ -48,11 +60,12 @@ class DiscStateContTimeModel(object):
         return self._asrh_hash != self._prev_asrv_hash
 
     def calc_q_mat(self):
-        raise NotImplemented()
+        raise NotImplementedError()
 
     def get_q_mat(self):
-        if q_mat_is_dirty:
-            self._calc_q_mat()
+        if self.q_mat_is_dirty():
+            self.calc_q_mat()
+            self._changed_params.clear()
         return self._q_mat
 
     def set_q_mat(self, v):
@@ -66,20 +79,16 @@ class DiscStateContTimeModel(object):
     q_mat = property(get_q_mat, set_q_mat)
     
     def get_q_mat_hash(self):
-        if q_mat_is_dirty():
+        if self.q_mat_is_dirty():
             self._q_mat_hash = hash(self.q_mat)
         return self._q_mat_hash
     q_mat_hash = property(get_q_mat_hash)
 
     def state_hash(self):
-        if  (self._total_state_hash is None) or  q_mat_is_dirty() or self.asrv_is_dirty():
+        if  (self._total_state_hash is None) or  self.q_mat_is_dirty() or self.asrv_is_dirty():
             self._total_state_hash = hash((id(self), self.q_mat_hash, self.asrv_hash))
             self._prev_asrv_hash = self._asrh_hash
         return self._total_state_hash
-        
-    def calc_prob_matrices(self, edge_len, eigen_soln_caching=None, prob_mat_caching=None):
-        self._incarnate()
-        return self._calc_prob_mat(self, edge_len, eigen_soln_caching=eigen_soln_caching, prob_mat_caching=prob_mat_caching)
         
 
     def _calc_prob_matrices(self, edge_len, eigen_soln_caching=None, prob_mat_caching=None):
@@ -152,17 +161,13 @@ class RevDiscStateContTimeModel(DiscStateContTimeModel):
         r_upper = kwargs.get('r_upper')
         r_mat = kwargs.get('r_mat')
         sf = kwargs.get('state_freq')
+        self.normalize_rates = kwargs.get('normalize_rates', True) # adjust the qmat such that the mean flux is 1.0
         if r_upper:
             if r_mat:
                 raise ValueError("r_mat or r_upper cannot both be specified")
             r_mat = _r_upper_to_r_mat(r_upper) 
         elif not r_mat:
             raise ValueError("Either r_mat or r_upper must be given")
-
-        if r_mat:
-            self._verify_r_mat(r_mat)
-        else:
-            raise ValueError("Either r_mat or r_upper must be specified")
 
         priv_mat = []
         param_set = set()
@@ -175,7 +180,6 @@ class RevDiscStateContTimeModel(DiscStateContTimeModel):
                     priv_row[n] = MutableFloatParameter(cell)
                 
             priv_mat.append(tuple(priv_row))
-        self._r_mat = tuple(priv_mat)
         if sf is not None:
             priv_sf = list(sf)
             for cell in sf:
@@ -183,20 +187,21 @@ class RevDiscStateContTimeModel(DiscStateContTimeModel):
                     param_set.update(cell.parameters())
                 except:
                     priv_sf.append(MutableFloatParameter(cell))
-            self.state_freq = tuple(priv_sf)
+            self._state_freq = tuple(priv_sf)
         else:
-            self.state_freq = None
+            self._state_freq = None
 
         DiscStateContTimeModel.__init__(self, param_list=param_set, **kwargs)
         if sf is None:
-            self.state_freq = tuple([1.0/self.num_states for i in xrange(self.num_states)])
+            self._state_freq = tuple([1.0/self.num_states for i in xrange(self.num_states)])
 
-    def get_q_mat(self):
-        "Returns a deep copy of the values in the Q-matrix."
-        q = []
-        for row in self._q_mat:
-            q.append(list(row))
-        return q
+        if r_mat:
+            self._verify_r_mat(r_mat)
+        else:
+            raise ValueError("Either r_mat or r_upper must be specified")
+        self._r_mat = tuple(priv_mat)
+
+
 
     def get_r_mat(self):
         "Accessor for R-matrix."
@@ -212,19 +217,25 @@ class RevDiscStateContTimeModel(DiscStateContTimeModel):
         return self._state_freq
     state_freq = property(get_state_freq)
 
+    q_mat = property(DiscStateContTimeModel.get_q_mat)
+
         
     
-    def _recalc_q_mat(self):
+    def calc_q_mat(self):
         """Uses self._state_freq and self._r_mat to refresh self._q_mat.
         
         As required by the current version of cdsctm_set_q_mat, the Q-matrix
             is rescaled such that each row sums to 0.0 and the weighted 
             average of the diagonal elements is -1.0 (with the weight being the
             associated equilibrium frequency)."""
-        _LOG.debug("in self._recalc_q_mat")
+        _LOG.debug("in self.calc_q_mat: _r_mat = %s _state_freq = %s" % (str(self._r_mat), str(self._state_freq)))
+        ns = self.num_states
+        qr = [0.0] * ns
+        qm = [list(qr) for i in range(ns)]
         w_mat_sum = 0.0
-        for i, rows in enumerate(izip(self._r_mat, self._q_mat)):
-            r_row, q_row = rows
+        for i, rows in enumerate(izip(self._r_mat, self._state_freq)):
+            r_row, sf = rows
+            q_row = qm[i]
             row_sum = 0.0
             for j, to_state in enumerate(izip(r_row, self._state_freq)):
                 r_val, stf = [float(p) for p in to_state]
@@ -236,12 +247,13 @@ class RevDiscStateContTimeModel(DiscStateContTimeModel):
                     row_sum += v
             q_row[i] = -row_sum
             w_mat_sum += row_sum*self._state_freq[i]
-        for q_row in self._q_mat:
-            for i in xrange(len(q_row)):
-                q_row[i] /= w_mat_sum
-        _LOG.debug("calling cdsctm_set_q_mat...")
-        cdsctm_set_q_mat(self._dsctm, self._q_mat)
-        _LOG.debug("back from cdsctm_set_q_mat...")
+        if self.normalize_rates:
+            for q_row in qm:
+                for i in xrange(len(q_row)):
+                    q_row[i] /= w_mat_sum
+        self._q_mat = tuple(qm)
+        _LOG.debug("_q_mat = %s" % str(qm))
+        return self._q_mat
     
     def _verify_eq_freq_len(self, equil_freq):
         "Raises a ValueError if `equil_freq` is not the correct length"
@@ -295,8 +307,8 @@ class JukesCantorModel(RevDiscStateContTimeModel):
     def __init__(self):
         dna = DNAType()
         _LOG.debug("Created dna type in JukesCantorModel")
-        RevDiscreteModel.__init__(self, r_upper=[[1.0, 1.0, 1.0], [1.0, 1.0], [1.0]], char_type=dna, params=[])
-        _LOG.debug("called RevDiscreteModel.__init__")
+        RevDiscStateContTimeModel.__init__(self, r_upper=[[1.0, 1.0, 1.0], [1.0, 1.0], [1.0]], char_type=dna, params=[])
+        _LOG.debug("called RevDiscStateContTimeModel.__init__")
 
 
 def _r_upper_to_r_mat(r_upper):
